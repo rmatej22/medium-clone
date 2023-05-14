e0e20526cbdf5ea4e07229dbe20e3115f1c36212
rmatej22 <rmatej545@gmail.com>
2023-05-14 20:35:34 +0200

    create actions folder and create register actions

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store, select } from '@ngrx/store';
 
-import { registerAction } from '../../store/actions';
+import { registerAction } from '../../store/register.action';
 import { Observable } from 'rxjs';
 import { AppStateInterface } from 'src/app/shared/types/appState.interface';
 import { isSubmittingSelector } from '../../store/selector';
--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -1,6 +1,6 @@
 import { createReducer, on } from '@ngrx/store';
 import { AuthStateInterface } from '../types/authState.interface';
-import { registerAction } from './actions';
+import { registerAction } from './register.action';
 
 const initialState: AuthStateInterface = {
   isSubmitting: false,
