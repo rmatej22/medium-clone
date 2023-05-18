d5939494bf0650fbbcbcf1b80fee711a55cac77a
rmatej22 <rmatej545@gmail.com>
2023-05-18 22:44:52 +0200

    get data from store for topBar and create navigation links

--- src/app/shared/modules/topBar/components/topBar/topBar.component.ts
+++ src/app/shared/modules/topBar/components/topBar/topBar.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import {
+  currentUserSelector,
+  isAnonymusSelector,
+  isLoggedInSelector,
+} from 'src/app/auth/store/selector';
+import { AppStateInterface } from 'src/app/shared/types/appState.interface';
+import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
 
 @Component({
   selector: 'mc-topbar',
   templateUrl: './topBar.component.html',
   styleUrls: ['./topBar.component.scss'],
 })
-export class TopBarComponent {}
+export class TopBarComponent implements OnInit {
+  isLoggedIn$: Observable<boolean>;
+  isAnonymus$: Observable<boolean>;
+  currentUser$: Observable<CurrentUserInterface | null>;
+
+  constructor(private store: Store<AppStateInterface>) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn$ = this.store.pipe(select(isLoggedInSelector));
+    this.isAnonymus$ = this.store.pipe(select(isAnonymusSelector));
+    this.currentUser$ = this.store.pipe(select(currentUserSelector));
+  }
+}
