e88ae1fb3def5e881717e83c6e0c009d0501e7be
rmatej22 <rmatej545@gmail.com>
2023-05-18 19:36:13 +0200

    create login interface and service method

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -6,6 +6,7 @@ import { RegisterRequestInterface } from '../types/registerRequest.interface';
 import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
 import { environment } from 'src/environments/environment';
 import { AuthResponseInterface } from '../types/authResponse.interface';
+import { LoginRequestInterface } from '../types/loginRequest.interface';
 
 @Injectable()
 export class AuthService {
@@ -13,6 +14,13 @@ export class AuthService {
 
   register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
     const url = `${environment.apiUrl}/users`;
+    return this.http
+      .post<AuthResponseInterface>(url, data)
+      .pipe(map((response: AuthResponseInterface) => response.user));
+  }
+
+  login(data: LoginRequestInterface): Observable<CurrentUserInterface> {
+    const url = `${environment.apiUrl}/users/login`;
     return this.http
       .post<AuthResponseInterface>(url, data)
       .pipe(map((response: AuthResponseInterface) => response.user));
