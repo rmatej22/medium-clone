5a1ee43d9ea1865c4c225b183a2e59502404ae88
rmatej22 <rmatej545@gmail.com>
2023-05-18 22:12:27 +0200

    create and implement topBar component

---
+++ src/app/shared/modules/topBar/components/topBar/topBar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mc-topbar',
+  templateUrl: './topBar.component.html',
+  styleUrls: ['./topBar.component.scss'],
+})
+export class TopBarComponent {}
