b423bbcc7002b6e365b08f419ae57cd06e0e26c2
rmatej22 <rmatej545@gmail.com>
2023-05-18 19:48:24 +0200

    create login actions & getUser helper method in auth.service

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -12,17 +12,21 @@ import { LoginRequestInterface } from '../types/loginRequest.interface';
 export class AuthService {
   constructor(private http: HttpClient) {}
 
+  getUser(response: AuthResponseInterface): CurrentUserInterface {
+    return response.user;
+  }
+
   register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
     const url = `${environment.apiUrl}/users`;
     return this.http
       .post<AuthResponseInterface>(url, data)
-      .pipe(map((response: AuthResponseInterface) => response.user));
+      .pipe(map(this.getUser));
   }
 
   login(data: LoginRequestInterface): Observable<CurrentUserInterface> {
     const url = `${environment.apiUrl}/users/login`;
     return this.http
       .post<AuthResponseInterface>(url, data)
-      .pipe(map((response: AuthResponseInterface) => response.user));
+      .pipe(map(this.getUser));
   }
 }
